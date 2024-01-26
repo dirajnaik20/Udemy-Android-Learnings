d253d119198314a0969d978c8760e2a0536d4abd
Diraj Naik <dirajnaik2020@gmail.com>
2024-01-26 23:08:17 +0530

    Half Way Through Subscriber repository

---
+++ app/src/main/java/com/example/androidlearning/db/SubscriberDAO.kt
@@ -0,0 +1,29 @@
+package com.example.androidlearning.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber)
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll(subscriber: Subscriber)
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers(): LiveData<List<String>>
+
+
+}
