25bab5d127f9f1636808813eb51a888fb5fb7e4a
Diraj Naik <dirajnaik2020@gmail.com>
2024-02-04 12:43:57 +0530

    ViewBinding Example

--- app/src/main/java/com/example/androidlearning/MainActivity.kt
+++ app/src/main/java/com/example/androidlearning/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSubmit.setOnClickListener {
+            displayGreetings()
+        }
+    }
+
+    private fun displayGreetings(){
+
+        binding.apply {
+            tvName.text="Hello! ${etName.text}"
+        }
+
     }
 }
